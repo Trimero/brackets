e3010935cbc105dff828c75f02263bbac145666e
Trimero <rusaxius@gmail.com>
2023-02-26 19:02:40 +0300

    init:end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let border = str.length;
+  bracketsConfig.forEach(a => str = str.split(a[0] + a[1]).join(''));
+    if (str.length === 0) {
+      return true 
+      } else if (str.length === border) {
+        return false 
+      } else return check(str, bracketsConfig)
+      }
